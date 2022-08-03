661daa28437ab2edfe613269a13c1c7c0311f107
Matheus Leal Gomes da Silva <matheusleal@tre-pa.jus.br>
2022-08-03 09:24:28 -0300

    refactor

--- src/main/java/com/control/e/modulo/colacao/rest/EventoRest.java
+++ src/main/java/com/control/e/modulo/colacao/rest/EventoRest.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/evento")
+@RequestMapping("/api/evento")
 public class EventoRest {
     @Autowired
     private EventoService service;
