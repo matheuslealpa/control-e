d9b091c55f1ed915ca9b5cae18898c70042afe3f
Matheus Leal <matheuslealpa@gmail.com>
2022-09-19 10:40:09 -0300

    Ajustes nas entidades

--- src/main/java/com/control/e/modulo/colacao/domain/Colando.java
+++ src/main/java/com/control/e/modulo/colacao/domain/Colando.java
@@ -3,7 +3,8 @@ package com.control.e.modulo.colacao.domain;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.*;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -12,11 +13,15 @@ public class Colando {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String matricula;
+
     private String nome;
+
     @ManyToOne
     private Curso curso;
-    @OneToMany
-    private List<Convidado> convidados;
+
+    @ElementCollection
+    private Set<String> convidados = new HashSet<>();
 
 }
